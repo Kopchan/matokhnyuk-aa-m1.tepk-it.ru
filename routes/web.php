9d763be84e99ec74b40c1dfda15ee861c2426375
Kopchan <83940856+Kopchan@users.noreply.github.com>
2025-06-07 13:37:57 +0700

    Заготовка шаблона

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+
+Route::resource('products', ProductController::class);
+Route::resource('partners', PartnerController::class);
