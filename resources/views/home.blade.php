6b4623a41ed39fe929c67c1bed2919519ff691e9
Kopchan <83940856+Kopchan@users.noreply.github.com>
2025-06-07 13:54:25 +0700

    Заготовка главной

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+@section('buttons')
+@endsection
+@section('content')
+    На главной ничего нет.
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\PartnerController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
 
-Route::resource('products', ProductController::class);
 Route::resource('partners', PartnerController::class);
