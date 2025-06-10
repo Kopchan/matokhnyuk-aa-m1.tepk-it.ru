8e3d856a4e23a8bf801a1914544b46b31ffc79f5
Kopchan <83940856+Kopchan@users.noreply.github.com>
2025-06-10 10:03:49 +0700

    Маршруты

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,18 @@
 <?php
 
-use App\Http\Controllers\PartnerController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
+// Главная страница
 Route::view('/', 'home')->name('home');
 
-Route::resource('partners', PartnerController::class);
+/* Ресурсный маршрут по CRUD продукта, создающий следующие маршруты:
+ * <Метод>   | <Название>       | <Описание>
+ * GET       | products.index   | страница списка продуктов
+ * GET       | products.show    | страница просмотра информации о продукте
+ * GET       | products.edit    | страница с формой редактирования конкретного продукта
+ * PUT|PATCH | products.update  | путь для формы на обновление данных конкретного продукта
+ * GET       | products.create  | страница с формой создания продукта
+ * POST      | products.store   | путь для формы на сохранение нового продукта
+ */
+Route::resource('products', ProductController::class);
