46700d2c5d84f400ffd8c390206560056ac2fa55
Kopchan <83940856+Kopchan@users.noreply.github.com>
2025-06-10 12:27:28 +0700

    Более информационная главная

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,10 @@
+@php use App\Models\Product; @endphp
 @extends('layout')
-@section('content', 'На главной ничего нет.')
+@section('content')
+    <div class="column gap12">
+        <a class="card" href="{{ route('products.index') }}">
+            <h3>Продукты ({{ Product::count() }})</h3>
+            <p>Просмотреть все</p>
+        </a>
+    </div>
+@endsection
